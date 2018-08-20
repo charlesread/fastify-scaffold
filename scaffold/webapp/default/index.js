6f7dd5248e48b7491d356bab57dbe66369c8560f
Charles Read <charles@charlesread.io>
2018-08-20 17:12:37 -0400

    catch sigint

--- scaffold/webapp/default/index.js
+++ scaffold/webapp/default/index.js
@@ -8,6 +8,10 @@ const log = require(path.join(__dirname, 'lib', 'logger.js'))()
 app.start()
   .then(function (fastify) {
     log.info('server listening on port %s', fastify.server.address().port)
+    process.on('SIGINT', function() {
+      log.info('SIGINT caught, shutting fastify down gracefully')
+      fastify.close(process.exit)
+    })
   })
   .catch(function (err) {
     console.error(err.stack)
